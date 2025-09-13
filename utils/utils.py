3f06c60c48334174fca66c7cc8a70af533a50625
Nicolas Madeo <nicomad.17@gmail.com>
2025-09-13 18:24:19 -0300

    Add utility functions for printing separators and empty lines

---
+++ utils/utils.py
@@ -0,0 +1,11 @@
+def print_separator():
+    """
+    Print a separator line.
+    """
+    print("================================================")
+
+def print_empty_line():
+    """
+    Print an empty line.
+    """
+    print()
